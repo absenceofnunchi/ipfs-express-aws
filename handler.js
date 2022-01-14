dbdde421e92d6e4c808b944722220537eff138e2
igibliss00@gmail.com <igibliss00@gmail.com>
2022-01-13 20:59:48 -0500

    Update ipfs-http-client version

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 const ipfsClient = require("ipfs-http-client");
 const app = express();
 
-const ipfs = ipfsClient({
+const ipfs = ipfsClient.create({
   host: "ipfs.infura.io",
   port: 5001,
   protocol: "https",
@@ -23,13 +23,7 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-app.post('/addImage', (req, res) => {
+app.post('/addImage', (req, res, next) => {
   var form = new multiparty.Form();
 
   form.parse(req, async function(err, fields, files) {
@@ -39,17 +33,17 @@ app.post('/addImage', (req, res) => {
     try {
       const added = await ipfs.add(data)
       console.log("added", added)
-      fs.unlinkSync(files.file[0].path);
+      // fs.unlinkSync(files.file[0].path);
       return res.status(200).send({ "ipfs success": added})
     } catch (err) {
       console.error(err)
-      fs.unlinkSync(files.file[0].path);
+      // fs.unlinkSync(files.file[0].path);
       return res.status(500).send({ "ipfs error": error})
     }
   })
 })
 
-app.post('/addFile', (req, res) => {
+app.post('/addFile', (req, res, next) => {
   var form = new multiparty.Form();
 
   form.parse(req, async function(err, fields, files) {
@@ -59,14 +53,20 @@ app.post('/addFile', (req, res) => {
     try {
       const added = await ipfs.add(data)
       console.log("added", added)
-      fs.unlinkSync(files.file[0].path);
+      // fs.unlinkSync(files.file[0].path);
       return res.status(200).send({ "ipfs success": added})
     } catch (err) {
       console.error(err)
-      fs.unlinkSync(files.file[0].path);
+      // fs.unlinkSync(files.file[0].path);
       return res.status(500).send({ "ipfs error": error})
     }    
   })
 })
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
 
 module.exports.handler = serverless(app);
